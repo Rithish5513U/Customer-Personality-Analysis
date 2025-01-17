fc3cec9cc7c8b17bf49786fd9ac742ee29dd55aa
Rithish S <rithish.s2022cce@sece.ac.in>
2025-01-17 13:27:50 +0530

    model update

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -14,52 +14,80 @@ class PreditctPipeline:
 
             model = load_object(file_path = model_path)
 
-            preds = model.predict()
+            preds = model.predict(features)
             return preds
         except Exception as e:
             raise CustomException(e, sys)
         
 class InputData:
     def __init__(self,
-        age: int,
+        education: str,
+        marital: str,
+        income: float,
+        wines: int,
+        fruits: int,
+        meat: int,
+        fish: int,
+        sweet: int,
+        gold: int,
+        camp: int,
+        purchases: int,
         children: int,
-        bmi: float,
-        sex: str,
-        smoker: str,
-        region: str):
+        ):
 
-        self.age = age
+        self.education = education
+        self.marital = marital
+        self.income = income
         self.children = children
-        self.bmi = bmi
-        self.sex = sex
-        self.smoker = smoker
-        self.region = region
+        self.wines = wines
+        self.fruits = fruits
+        self.fish = fish
+        self.meat = meat
+        self.sweet = sweet
+        self.gold = gold
+        self.purchases = purchases
+        self.camp = camp
 
     def get_data_as_dataFrame(self):
         try:
             input_dict = {
-                "age": [self.age],
-                "children": [self.children],
-                "bmi": [self.bmi],
-                "sex": [self.sex],
-                "smoker": [self.smoker],
-                "region": [self.region]
+                "Education": [self.education],
+                "Marital_Status": [self.marital],
+                "Income": [self.income],
+                "Wines": [self.wines],
+                "Fruits": [self.fruits],
+                "Meat": [self.meat],
+                "Fish": [self.fish],
+                "Sweets": [self.sweet],
+                "Gold": [self.gold],
+                "camp": [self.camp],
+                "Purchases": [self.purchases],
+                "Children": [self.children],
             }
 
             convert_dict = {
-                'age': int,
-                'children': int,
-                'bmi': float,
-                'sex': str,
-                'smoker': str,
-                'region': str
+                'Year_Birth': int,
+                'Education': str,
+                'Marital_Status': str,
+                'Income': float,
+                'Wines': int,
+                'Fruits': int,
+                'Meat': int,
+                'Fish': int,
+                'Sweets': int,
+                'Gold': int,
+                'camp': int,
+                'Purchases': int,
+                'Children': int,
             }
 
             df = pd.DataFrame(input_dict)
 
             df = df.astype(convert_dict)
-            print(df.info())
-
+            df['Education']=df['Education'].replace({'Basic':0,'2n Cycle':0,'Graduation':1,'Master':1,'PhD':1})
+            df['Marital_Status']=df['Marital_Status'].replace({'Divorced':0,'Single':0,'Married':1,'Together':1,'Absurd':0,'Widow':0,'YOLO':0})
+            df['Expenses']=df['Wines']+df['Fruits']+df['Fish']+df['Gold']+df['Meat']+df['Sweets']
+            
             return df
 
         except Exception as e:
